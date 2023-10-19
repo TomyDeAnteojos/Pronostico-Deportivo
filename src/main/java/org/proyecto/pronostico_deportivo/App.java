c4a074d85bac5b2cbd50b0fa370ed2b141dd01db
Tomas Medel <tfmedel@gmail.com>
2023-10-19 16:33:24 -0300

    proyecto creado

---
+++ src/main/java/org/proyecto/pronostico_deportivo/App.java
@@ -0,0 +1,13 @@
+package org.proyecto.pronostico_deportivo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
