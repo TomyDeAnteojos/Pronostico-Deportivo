499d273f9bda9ff89b3257d030df0fd484e4bcb8
Tomas Medel <tfmedel@gmail.com>
2023-10-24 16:55:09 -0300

    creando el proyecto

---
+++ src/main/java/org/grupo_g/pronostico_deportivo/App.java
@@ -0,0 +1,13 @@
+package org.grupo_g.pronostico_deportivo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
