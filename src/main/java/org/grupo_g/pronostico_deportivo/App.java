25e29b06ea381f4fd07c017cc6b1b3ae073eb464
Tomas Medel <tfmedel@gmail.com>
2023-10-28 11:58:10 -0300

    menu añadido

--- src/main/java/org/grupo_g/pronostico_deportivo/App.java
+++ src/main/java/org/grupo_g/pronostico_deportivo/App.java
@@ -1,13 +1,31 @@
 package org.grupo_g.pronostico_deportivo;
 
-/**
- * Hello world!
- *
- */
+import Servicios.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        while(true)
+        {
+            switch (Servicios.Menu.menuOpciones())
+            {
+                case 1:
+                    // Cargar equipos
+                    break;
+                case 2:
+                    // Empezar
+                    // Ingresar apuestas
+                    // ingresar goles
+                    // calcular puntaje
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
